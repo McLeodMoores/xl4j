9f8bf11235caaca1331d9490056184d52cde9a4d
Elaine McLeod <em.mcleod@gmail.com>
2014-07-21 23:12:09 +0100

    Testing the Integer <-> XLNumber converter

---
+++ src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/IntegerXLNumberTypeConverterTest.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (C) 2014-Present McLeod Moores Software Limited.  All rights reserved.
+ */
+package com.mcleodmoores.excel4j.typeconvert.converters;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.mcleodmoores.excel4j.typeconvert.AbstractTypeConverter;
+import com.mcleodmoores.excel4j.typeconvert.ExcelToJavaTypeMapping;
+import com.mcleodmoores.excel4j.typeconvert.JavaToExcelTypeMapping;
+import com.mcleodmoores.excel4j.values.XLInteger;
+import com.mcleodmoores.excel4j.values.XLNumber;
+import com.mcleodmoores.excel4j.values.XLValue;
+
+/**
+ * Unit tests for {@link IntegerXLNumberTypeConverter}.
+ */
+@Test
+public class IntegerXLNumberTypeConverterTest {
+  // REVIEW isn't it a bit odd that there's no complaint when there's a downcast to Integer?
+  /** XLNumber holding a double. */
+  private static final XLNumber XL_NUMBER_DOUBLE = XLNumber.of(10.);
+  /** XLNumber holding a long. */
+  private static final XLNumber XL_NUMBER_LONG = XLNumber.of(10L);
+  /** XLNumber holding an int. */
+  private static final XLNumber XL_NUMBER_INT = XLNumber.of(10);
+  /** Integer. */
+  private static final Integer INTEGER = 10;
+  /** The converter. */
+  private static final AbstractTypeConverter CONVERTER = new IntegerXLNumberTypeConverter();
+
+  /**
+   * Tests that the java class is {@link Integer#TYPE}.
+   */
+  @Test
+  public void testGetExcelToJavaTypeMapping() {
+    assertEquals(CONVERTER.getExcelToJavaTypeMapping(), ExcelToJavaTypeMapping.of(XLNumber.class, Integer.TYPE));
+  }
+
+  /**
+   * Tests that the excel type is {@link XLNumber}.
+   */
+  @Test
+  public void testGetJavaToExcelTypeMapping() {
+    assertEquals(CONVERTER.getJavaToExcelTypeMapping(), JavaToExcelTypeMapping.of(Integer.TYPE, XLNumber.class));
+  }
+
+  /**
+   * Tests the expected priority.
+   */
+  @Test
+  public void testPriority() {
+    assertEquals(CONVERTER.getPriority(), 10);
+  }
+
+  /**
+   * Tests that passing in a null expected {@link XLValue} class gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullExpectedXLValueClass() {
+    CONVERTER.toXLValue(null, INTEGER);
+  }
+
+  /**
+   * Tests that passing in a null object gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullObject() {
+    CONVERTER.toXLValue(XLNumber.class, null);
+  }
+
+  /**
+   * Tests that passing in a null expected Java class gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullExpectedClass() {
+    CONVERTER.toJavaObject(null, XL_NUMBER_DOUBLE);
+  }
+
+  /**
+   * Tests that passing in a null object gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullXLValue() {
+    CONVERTER.toJavaObject(Integer.class, null);
+  }
+
+  /**
+   * Tests for the exception when the object to convert is the wrong type.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongTypeToJavaConversion() {
+    CONVERTER.toJavaObject(Integer.TYPE, XLInteger.of(10));
+  }
+
+  /**
+   * Tests for the exception when the expected class is wrong.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongExpectedClassToJavaConversion() {
+    CONVERTER.toJavaObject(Long.class, XLNumber.of(10.));
+  }
+
+  /**
+   * Tests for the exception when {@link XLValue} to convert is the wrong type.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongTypeToXLConversion() {
+    CONVERTER.toXLValue(XLNumber.class, 10L);
+  }
+
+  /**
+   * Tests for the exception when the expected class is wrong.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongExpectedClassToXLConversion() {
+    CONVERTER.toXLValue(XLValue.class, 1);
+  }
+
+  /**
+   * Tests the conversion from a {@link Integer}.
+   */
+  @Test
+  public void testConversionFromInteger() {
+    final XLValue converted = CONVERTER.toXLValue(XL_NUMBER_INT.getClass(), INTEGER);
+    assertTrue(converted instanceof XLNumber);
+    final XLNumber xlNumber = (XLNumber) converted;
+    assertEquals(xlNumber.getValue(), 10, 0);
+  }
+
+  /**
+   * Tests the conversion from a {@link XLNumber}.
+   */
+  @Test
+  public void testConversionFromXLNumber() {
+    Object converted = CONVERTER.toJavaObject(Integer.class, XL_NUMBER_INT);
+    assertTrue(converted instanceof Integer);
+    Integer integ = (Integer) converted;
+    assertEquals(integ, INTEGER);
+    converted = CONVERTER.toJavaObject(Integer.class, XL_NUMBER_LONG);
+    assertTrue(converted instanceof Integer);
+    integ = (Integer) converted;
+    assertEquals(integ, INTEGER);
+    converted = CONVERTER.toJavaObject(Integer.class, XL_NUMBER_DOUBLE);
+    assertTrue(converted instanceof Integer);
+    integ = (Integer) converted;
+    assertEquals(integ, INTEGER);
+  }
+}
