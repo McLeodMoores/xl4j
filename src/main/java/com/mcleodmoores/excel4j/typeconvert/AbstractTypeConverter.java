7c3ea2554cb7ae02aa5d1d008a05c55f3616aa62
Elaine McLeod <em.mcleod@gmail.com>
2014-07-21 22:45:12 +0100

    Adding review comments

--- src/main/java/com/mcleodmoores/excel4j/typeconvert/AbstractTypeConverter.java
+++ src/main/java/com/mcleodmoores/excel4j/typeconvert/AbstractTypeConverter.java
@@ -1,58 +1,73 @@
 package com.mcleodmoores.excel4j.typeconvert;
-import com.mcleodmoores.excel4j.values.XLValue;
 
+import com.mcleodmoores.excel4j.values.XLValue;
 
 /**
  * Base class for type converters, removes need for some boilerplate.
  */
 public abstract class AbstractTypeConverter implements TypeConverter {
-
+  // REVIEW emcleod 21-7-2014 No input checks - NullPointerExceptions are
+  // wanted?
   private static final int DEFAULT_PRIORITY = 10;
-  
-  private ExcelToJavaTypeMapping _excelToJavaTypeMapping;
-  private JavaToExcelTypeMapping _javaToExcelTypeMapping;
-  private int _priority;
 
+  private final ExcelToJavaTypeMapping _excelToJavaTypeMapping;
+  private final JavaToExcelTypeMapping _javaToExcelTypeMapping;
+  private final int _priority;
 
   /**
    * Convenience constructor, produces converter with default priority.
-   * @param javaType the Java type, any object type
-   * @param excelType the Excel type, subclass of XLValue
-   * @param priority the priority level, with larger values indicating higher priority
+   *
+   * @param javaType
+   *          the Java type, any object type
+   * @param excelType
+   *          the Excel type, subclass of XLValue
+   * @param priority
+   *          the priority level, with larger values indicating higher priority
    */
   protected AbstractTypeConverter(final Class<?> javaType, final Class<? extends XLValue> excelType, final int priority) {
     _excelToJavaTypeMapping = ExcelToJavaTypeMapping.of(excelType, javaType);
     _javaToExcelTypeMapping = JavaToExcelTypeMapping.of(javaType, excelType);
     _priority = priority;
   }
-  
+
   /**
    * Convenience constructor, produces converter with default priority.
-   * @param javaType the Java type, any object type
-   * @param excelType the Excel type, subclass of XLValue
+   *
+   * @param javaType
+   *          the Java type, any object type
+   * @param excelType
+   *          the Excel type, subclass of XLValue
    */
   protected AbstractTypeConverter(final Class<?> javaType, final Class<? extends XLValue> excelType) {
     this(javaType, excelType, DEFAULT_PRIORITY);
   }
-  
+
   @Override
   public ExcelToJavaTypeMapping getExcelToJavaTypeMapping() {
     return _excelToJavaTypeMapping;
   }
-  
+
   @Override
   public JavaToExcelTypeMapping getJavaToExcelTypeMapping() {
     return _javaToExcelTypeMapping;
   }
 
+  // REVIEW emcleod 21-7-2014 - expecting ClassCastException?
+  // expectedClass doesn't appear to be used. Isn't it redundant
+  // anyway given the type mapping instance variables?
   @Override
   public abstract XLValue toXLValue(Class<? extends XLValue> expectedClass, Object from);
-  
+
+  // REVIEW emcleod 21-7-2014 - expecting ClassCastException?
+  // expectedClass doesn't appear to be used. Isn't it redundant
+  // anyway given the type mapping instance variables?
   @Override
   public abstract Object toJavaObject(Class<?> expectedClass, XLValue from);
-  
+
   @Override
   public int getPriority() {
     return _priority;
   }
+
+  // REVIEW emcleod 21-7-2014 hashCode and equals?
 }
