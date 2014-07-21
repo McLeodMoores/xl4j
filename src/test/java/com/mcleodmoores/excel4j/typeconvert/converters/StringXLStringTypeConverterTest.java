ea36e430fd51da93b9bb9cf07204e4ad279aea1e
Elaine McLeod <em.mcleod@gmail.com>
2014-07-21 23:30:19 +0100

    Testing the String <-> XLString converter

---
+++ src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/StringXLStringTypeConverterTest.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2014-Present McLeod Moores Software Limited.  All rights reserved.
+ */
+package com.mcleodmoores.excel4j.typeconvert.converters;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.mcleodmoores.excel4j.typeconvert.AbstractTypeConverter;
+import com.mcleodmoores.excel4j.typeconvert.ExcelToJavaTypeMapping;
+import com.mcleodmoores.excel4j.typeconvert.JavaToExcelTypeMapping;
+import com.mcleodmoores.excel4j.util.Excel4JRuntimeException;
+import com.mcleodmoores.excel4j.values.XLNumber;
+import com.mcleodmoores.excel4j.values.XLString;
+import com.mcleodmoores.excel4j.values.XLValue;
+
+/**
+ * Unit tests for {@link StringXLStringTypeConverter}.
+ */
+@Test
+public class StringXLStringTypeConverterTest {
+  /** XLString. */
+  private static final XLString XL_STRING = XLString.of("TEST");
+  /** String. */
+  private static final String STRING = "TEST";
+  /** The converter */
+  private static final AbstractTypeConverter CONVERTER = new StringXLStringTypeConverter();
+
+  /**
+   * Tests that the java type is {@link String}.
+   */
+  @Test
+  public void testGetExcelToJavaTypeMapping() {
+    assertEquals(CONVERTER.getExcelToJavaTypeMapping(), ExcelToJavaTypeMapping.of(XLString.class, String.class));
+  }
+
+  /**
+   * Tests that the excel type is {@link XLString}.
+   */
+  @Test
+  public void testGetJavaToExcelTypeMapping() {
+    assertEquals(CONVERTER.getJavaToExcelTypeMapping(), JavaToExcelTypeMapping.of(String.class, XLString.class));
+  }
+
+  /**
+   * Tests the expected priority.
+   */
+  @Test
+  public void testPriority() {
+    assertEquals(CONVERTER.getPriority(), 10);
+  }
+
+  /**
+   * Tests that passing in a null expected {@link XLValue} class gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullExpectedXLValueClass() {
+    CONVERTER.toXLValue(null, STRING);
+  }
+
+  /**
+   * Tests that passing in a null object gives the expected exception.
+   */
+  @Test(expectedExceptions = Excel4JRuntimeException.class)
+  public void testNullObject() {
+    CONVERTER.toXLValue(XLString.class, null);
+  }
+
+  /**
+   * Tests that passing in a null expected Java class gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullExpectedClass() {
+    CONVERTER.toJavaObject(null, XL_STRING);
+  }
+
+  /**
+   * Tests that passing in a null object gives the expected exception.
+   */
+  @Test(expectedExceptions = NullPointerException.class)
+  public void testNullXLValue() {
+    CONVERTER.toJavaObject(String.class, null);
+  }
+
+  /**
+   * Tests for the exception when the object to convert is the wrong type.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongTypeToJavaConversion() {
+    CONVERTER.toJavaObject(String.class, XLNumber.of(10.));
+  }
+
+  /**
+   * Tests for the exception when the expected class is wrong.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongExpectedClassToJavaConversion() {
+    CONVERTER.toJavaObject(Double.class, XL_STRING);
+  }
+
+  /**
+   * Tests for the exception when {@link XLValue} to convert is the wrong type.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongTypeToXLConversion() {
+    CONVERTER.toXLValue(XLString.class, 10.);
+  }
+
+  /**
+   * Tests for the exception when the expected class is wrong.
+   */
+  @Test(expectedExceptions = ClassCastException.class)
+  public void testWrongExpectedClassToXLConversion() {
+    CONVERTER.toXLValue(XLNumber.class, STRING);
+  }
+
+  /**
+   * Tests the conversion from a {@link String}.
+   */
+  @Test
+  public void testConversionFromString() {
+    final XLValue converted = CONVERTER.toXLValue(XL_STRING.getClass(), STRING);
+    assertTrue(converted instanceof XLString);
+    final XLString xlString = (XLString) converted;
+    assertEquals(xlString.getValue(), STRING);
+  }
+
+  /**
+   * Tests the conversion from a {@link XLString}.
+   */
+  @Test
+  public void testConversionFromXLString() {
+    final Object converted = CONVERTER.toJavaObject(String.class, XL_STRING);
+    assertTrue(converted instanceof String);
+    final String str = (String) converted;
+    assertEquals(str, STRING);
+  }
+
+}
