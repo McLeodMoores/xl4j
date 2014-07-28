49559800f9c0d3ec9248f5bd032c7bad33dc97c1
Andrew Griffin <aiwg@beerdragon.co.uk>
2014-07-28 22:09:17 +0100

    Fixes #14 Changes to the build scripts.
    Fix to the file copying stage which might have resulted in a file instead of an intermediate file directory.
    Compilation error of core-test from absence of `assert` in default header chain.
    Ensure that proxy stubs are generated by moving interface delcs out of library, but referencing them from within.

--- comjvm/copy-dlls.bat
+++ comjvm/copy-dlls.bat
@@ -1,3 +1,5 @@
+mkdir "%1\target\%3-Ascii-%4"
+mkdir "%1\target\%3-Unicode-%4"
 copy /y "%1\target\%3-%4\%2.dll" "%1\target\%3-Ascii-%4"
 copy /y "%1\target\%3-%4\%2.xml" "%1\target\%3-Ascii-%4"
 copy /y "%1\target\%3-%4\*.h" "%1\target\%3-Ascii-%4"
