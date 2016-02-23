d9c2277eff3804a0f59f4bdfdc4ce370bf12b02c
Elaine McLeod <em.mcleod@gmail.com>
2016-02-23 17:14:46 +0000

    Adding a clearer error message

--- src/main/java/com/mcleodmoores/excel4j/javacode/ObjectConstructorInvoker.java
+++ src/main/java/com/mcleodmoores/excel4j/javacode/ObjectConstructorInvoker.java
@@ -8,32 +8,32 @@ import com.mcleodmoores.excel4j.util.Excel4JRuntimeException;
 import com.mcleodmoores.excel4j.values.XLValue;
 
 /**
- * A class holding the converters required to convert arguments into the appropriate types and convert the result. 
+ * A class holding the converters required to convert arguments into the appropriate types and convert the result.
  */
 public class ObjectConstructorInvoker implements ConstructorInvoker {
-  private Constructor<?> _constructor;
-  private TypeConverter[] _argumentConverters;
-  private TypeConverter _returnConverter;
-  
+  private final Constructor<?> _constructor;
+  private final TypeConverter[] _argumentConverters;
+  private final TypeConverter _returnConverter;
+
   /**
    * Constructor.
    * @param constructor the constructor to call.
    * @param argumentConverters the converters required to call the method
    * @param returnConverter the converter required to convert he result back to an Excel type
    */
-  public ObjectConstructorInvoker(final Constructor<?> constructor, final TypeConverter[] argumentConverters, 
-                            final TypeConverter returnConverter) {
+  public ObjectConstructorInvoker(final Constructor<?> constructor, final TypeConverter[] argumentConverters,
+      final TypeConverter returnConverter) {
     _constructor = constructor;
     _argumentConverters = argumentConverters;
     _returnConverter = returnConverter;
   }
-  
+
   /* (non-Javadoc)
    * @see com.mcleodmoores.excel4j.javacode.ConstructorInvokerI#invoke(com.mcleodmoores.excel4j.values.XLValue[])
    */
   @Override
   public XLValue invoke(final XLValue[] arguments) {
-    Object[] args = new Object[arguments.length];
+    final Object[] args = new Object[arguments.length];
     for (int i = 0; i < _argumentConverters.length; i++) {
       if (arguments[i].getClass().isArray()) {
         args[i] = arguments[i];
@@ -42,31 +42,31 @@ public class ObjectConstructorInvoker implements ConstructorInvoker {
       }
     }
     try {
-      Object result = _constructor.newInstance(args);
+      final Object result = _constructor.newInstance(args);
       if (result.getClass().isArray()) {
         throw new Excel4JRuntimeException("Return of array types not supported");
       } else {
         return (XLValue) _returnConverter.toXLValue(null, result);
       }
-     } catch (IllegalAccessException | IllegalArgumentException
+    } catch (IllegalAccessException | IllegalArgumentException
         | InvocationTargetException | InstantiationException e) {
-      throw new Excel4JRuntimeException("Error invoking constructor", e);
+      throw new Excel4JRuntimeException("Error invoking constructor: " + e.getMessage(), e);
     }
   }
-  
+
   /* (non-Javadoc)
    * @see com.mcleodmoores.excel4j.javacode.ConstructorInvokerI#getExcelParameterTypes()
    */
   @Override
   public Class<?>[] getExcelParameterTypes() {
-    Class<?>[] parameterTypes = new Class[_argumentConverters.length];
-    int i = 0;
-    for (TypeConverter typeConverter : _argumentConverters) {
-      parameterTypes[i] = typeConverter.getJavaToExcelTypeMapping().getExcelClass(); 
+    final Class<?>[] parameterTypes = new Class[_argumentConverters.length];
+    final int i = 0;
+    for (final TypeConverter typeConverter : _argumentConverters) {
+      parameterTypes[i] = typeConverter.getJavaToExcelTypeMapping().getExcelClass();
     }
     return parameterTypes;
   }
-  
+
   /* (non-Javadoc)
    * @see com.mcleodmoores.excel4j.javacode.ConstructorInvokerI#getExcelReturnType()
    */
@@ -74,5 +74,5 @@ public class ObjectConstructorInvoker implements ConstructorInvoker {
   public Class<?> getExcelReturnType() {
     return _returnConverter.getJavaToExcelTypeMapping().getExcelClass();
   }
-  
+
 }
