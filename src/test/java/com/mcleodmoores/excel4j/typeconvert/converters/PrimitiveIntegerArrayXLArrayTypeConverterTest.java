1d6101013a39e82a7926c30349477fdc793fb4c3
Elaine McLeod <em.mcleod@gmail.com>
2016-03-02 17:27:23 +0000

    Adding a String -> integer converter. Closes #35

--- src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/PrimitiveIntegerArrayXLArrayTypeConverterTest.java
+++ src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/PrimitiveIntegerArrayXLArrayTypeConverterTest.java
@@ -15,6 +15,7 @@ import com.mcleodmoores.excel4j.typeconvert.ExcelToJavaTypeMapping;
 import com.mcleodmoores.excel4j.typeconvert.JavaToExcelTypeMapping;
 import com.mcleodmoores.excel4j.util.Excel4JRuntimeException;
 import com.mcleodmoores.excel4j.values.XLArray;
+import com.mcleodmoores.excel4j.values.XLBoolean;
 import com.mcleodmoores.excel4j.values.XLInteger;
 import com.mcleodmoores.excel4j.values.XLNumber;
 import com.mcleodmoores.excel4j.values.XLString;
@@ -101,7 +102,7 @@ public class PrimitiveIntegerArrayXLArrayTypeConverterTest {
    */
   @Test(expectedExceptions = Excel4JRuntimeException.class)
   public void testWrongComponentTypeToJavaConversion() {
-    CONVERTER.toJavaObject(int[].class, XLArray.of(new XLValue[][] {new XLValue[] {XLNumber.of(10), XLNumber.of(20)}}));
+    CONVERTER.toJavaObject(boolean[].class, XLArray.of(new XLValue[][] {new XLValue[] {XLNumber.of(10), XLNumber.of(20)}}));
   }
 
   /**
@@ -118,6 +119,24 @@ public class PrimitiveIntegerArrayXLArrayTypeConverterTest {
   @Test(expectedExceptions = Excel4JRuntimeException.class)
   public void testExpectedTypeNotAnArray() {
     CONVERTER.toXLValue(XLInteger.class, new int[] {10, 20});
+  }
+
+  /**
+   * Tests the behaviour when there is no available converter to a int.
+   */
+  @Test(expectedExceptions = Excel4JRuntimeException.class)
+  public void testNoConverterToFloatRow() {
+    final XLArray xlArray = XLArray.of(new XLValue[][] {new XLValue[] {XLBoolean.FALSE, XLBoolean.FALSE}});
+    CONVERTER.toJavaObject(int[].class, xlArray);
+  }
+
+  /**
+   * Tests the behaviour when there is no available converter to a int.
+   */
+  @Test(expectedExceptions = Excel4JRuntimeException.class)
+  public void testNoConverterToFloatColumn() {
+    final XLArray xlArray = XLArray.of(new XLValue[][] {new XLValue[] {XLBoolean.FALSE}, new XLValue[]{XLBoolean.FALSE}});
+    CONVERTER.toJavaObject(int[].class, xlArray);
   }
 
   /**
