083728e277ddad3d8dc3501cf9b00d5fcfd67183
Elaine McLeod <em.mcleod@gmail.com>
2016-03-10 15:54:00 +0000

    Fixed the test. Closes #19

--- src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/LocalDateConstructionTest.java
+++ src/test/java/com/mcleodmoores/excel4j/typeconvert/converters/LocalDateConstructionTest.java
@@ -20,15 +20,13 @@ import com.mcleodmoores.excel4j.values.XLValue;
  * Tests construction of LocalDates from the function processor.
  */
 public class LocalDateConstructionTest extends TypeConstructionTests {
-  private static final int TWO_THOUSAND = 2000;
-  private static final int EXCEL_EPOCH_YEAR = 1900;
-  /** The number of days from the Excel epoch */
-  private static final long DAYS_FROM_EXCEL_EPOCH = ChronoUnit.DAYS.between(LocalDate.of(EXCEL_EPOCH_YEAR, 1, 1),
+  /** The number of days from the Excel epoch to LocalDate epoch */
+  private static final long DAYS_FROM_EXCEL_EPOCH = ChronoUnit.DAYS.between(LocalDate.of(1900, 1, 1),
       LocalDate.ofEpochDay(0)) + 1;
   /** The number of days from Excel epoch to 2000-01-01 */
-  private static final long DAYS = LocalDate.of(TWO_THOUSAND, 1, 1).toEpochDay() + DAYS_FROM_EXCEL_EPOCH;
+  private static final long DAYS = LocalDate.of(2000, 1, 1).toEpochDay() + DAYS_FROM_EXCEL_EPOCH;
   /** XLNumber holding a double representing 2000-01-01. */
-  private static final XLNumber XL_DATE = XLNumber.of(DAYS);
+  private static final XLNumber XL_DATE = XLNumber.of(DAYS - DAYS_FROM_EXCEL_EPOCH);
   /** Local date. */
   private static final LocalDate LOCAL_DATE = LocalDate.of(2000, 1, 1);
   /** The class name */
