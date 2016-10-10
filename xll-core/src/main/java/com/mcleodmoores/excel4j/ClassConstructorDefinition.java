e5a884a57ba9e7afb72a0529174471d99107ca09
Elaine McLeod <em.mcleod@gmail.com>
2016-10-10 15:55:45 +0100

    Adding a constructor number for classes with a XLClass annotation

--- xll-core/src/main/java/com/mcleodmoores/excel4j/ClassConstructorDefinition.java
+++ xll-core/src/main/java/com/mcleodmoores/excel4j/ClassConstructorDefinition.java
@@ -17,6 +17,8 @@ public final class ClassConstructorDefinition {
   private final ClassMetadata _classMetadata;
   /** The constructor invoker */
   private final ConstructorInvoker _constructorInvoker;
+  /** The number of the constructor */
+  private final Integer _constructorNumber;
   /** Constructor export number in registry */
   private final int _exportNumber;
 
@@ -24,16 +26,20 @@ public final class ClassConstructorDefinition {
    * Creates an instance.
    * @param classMetadata  the meta-data
    * @param constructorInvoker  the constructor invoker
+   * @param the constructorNumber  the constructor number
+   * @param constructorNumber  the constructor number
    * @param exportNumber  the export number
    */
-  private ClassConstructorDefinition(final ClassMetadata classMetadata, final ConstructorInvoker constructorInvoker, final int exportNumber) {
+  private ClassConstructorDefinition(final ClassMetadata classMetadata, final ConstructorInvoker constructorInvoker, final Integer constructorNumber,
+      final int exportNumber) {
     _classMetadata = classMetadata;
     _constructorInvoker = constructorInvoker;
+    _constructorNumber = constructorNumber;
     _exportNumber = exportNumber;
   }
 
   /**
-   * Static factory method to create an instance.
+   * Static factory method to create an instance used where there is only one constructor (so the constructor number is not set).
    * @param classMetadata  the annotation-based meta-data about the function, not null
    * @param constructorInvoker  the type conversion and new instance binding for this constructor, not null
    * @param exportNumber  the number of the DLL export that handles this constructor (only unique to the number of parameters used)
@@ -42,7 +48,24 @@ public final class ClassConstructorDefinition {
   public static ClassConstructorDefinition of(final ClassMetadata classMetadata, final ConstructorInvoker constructorInvoker, final int exportNumber) {
     ArgumentChecker.notNull(classMetadata, "classMetadata");
     ArgumentChecker.notNull(constructorInvoker, "constructorInvoker");
-    return new ClassConstructorDefinition(classMetadata, constructorInvoker, exportNumber);
+    return new ClassConstructorDefinition(classMetadata, constructorInvoker, null, exportNumber);
+  }
+
+  /**
+   * Static factory method to create an instance used where there is more than one constructor available for this class. In this case,
+   * $[#] will be appended to the class name.
+   * @param classMetadata  the annotation-based meta-data about the function, not null
+   * @param constructorInvoker  the type conversion and new instance binding for this constructor, not null
+   * @param constructorNumber  the constructor number, not null
+   * @param exportNumber  the number of the DLL export that handles this constructor (only unique to the number of parameters used)
+   * @return  an instance of this constructor definition
+   */
+  public static ClassConstructorDefinition of(final ClassMetadata classMetadata, final ConstructorInvoker constructorInvoker, final Integer constructorNumber,
+      final int exportNumber) {
+    ArgumentChecker.notNull(classMetadata, "classMetadata");
+    ArgumentChecker.notNull(constructorInvoker, "constructorInvoker");
+    ArgumentChecker.notNull(constructorNumber, "constructorNumber");
+    return new ClassConstructorDefinition(classMetadata, constructorInvoker, constructorNumber, exportNumber);
   }
 
   /**
@@ -57,6 +80,13 @@ public final class ClassConstructorDefinition {
    */
   public ConstructorInvoker getConstructorInvoker() {
     return _constructorInvoker;
+  }
+
+  /**
+   * @return  the constructor number, can be null
+   */
+  public Integer getConstructorNumber() {
+    return _constructorNumber;
   }
 
   /**
